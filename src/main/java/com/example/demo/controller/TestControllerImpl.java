eb334902de33d297759292606694eb136b801979
kvkvkvkvkv <kvsreenidhi@gmail.com>
2024-12-22 17:37:55 +0530

    returning json

--- src/main/java/com/example/demo/controller/TestControllerImpl.java
+++ src/main/java/com/example/demo/controller/TestControllerImpl.java
@@ -1,8 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.Test;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +12,12 @@ public class TestControllerImpl implements TestController {
     @GetMapping("test")
     public String test() {
         return "Hiii";
+    }
+
+    @GetMapping("test-bean")
+    public Test testBean() {
+        Test test = new Test();
+        test.setMessage("test");
+        return test;
     }
 }
---
+++ src/main/java/com/example/demo/dto/Test.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class Test {
+    private String message;
+
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+}
